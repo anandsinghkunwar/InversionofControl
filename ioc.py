5faf4b0ac1b8d2f5e2891fb588b74215d65dabf3
Anand Singh Kunwar <anandkunwar95@gmail.com>
2016-10-27 17:01:27 +0530

    Added IoC Example as a Graphics Engine

---
+++ ioc.py
@@ -0,0 +1,50 @@
+import time
+class Frame(object):
+    def __init__(self, Frame):
+        self.Frame = Frame
+
+class Engine(object):
+    def __init__(self):
+        # Default Frame Rate is 1 fps
+        self.FrameRate = 24
+        self.Scene = []
+        self.Loop = True
+    
+    def AddFrame(self, frame):
+    """For Adding a Frame to the Scene"""
+        self.Scene.append(frame)
+
+    def RenderFrame(self, frame):
+    """A Crude Frame Renderer"""
+        print frame.Frame
+
+    def Render(self):
+    """Render Function to be called by """
+        while True:
+            for frame in self.Scene:
+                time.sleep(1.0/self.FrameRate)
+                print '\n'*50   # Clearing Frame Hack
+                self.RenderFrame(frame)
+            if not self.Loop:
+                break
+
+engine = Engine()
+ROFLCopter1 = '''
+          ROFL:ROFL:LOL:
+                    _^___
+           L    __/   [] \    
+           O ===__        \ 
+           L      \________]
+                    I   I
+                   --------/'''
+ROFLCopter2 = '''
+                   :LOL:ROFL:ROFL
+                    _^___
+                __/   [] \    
+          LOL===__        \ 
+                  \________]
+                    I   I
+                   --------/'''
+engine.AddFrame(Frame(ROFLCopter1))
+engine.AddFrame(Frame(ROFLCopter2))
+engine.Render()
